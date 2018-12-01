192c91556554f4da134537d3730e7d0f42cc2dd4
Dmytro Platov <brassack@gmail.com>
2018-12-01 12:46:08 +0200

    Initial Commit

---
+++ Livepc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Livepc
+//
+//  Created by Dmytro Platov on 12/1/18.
+//  Copyright © 2018 Dmytro Platov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
